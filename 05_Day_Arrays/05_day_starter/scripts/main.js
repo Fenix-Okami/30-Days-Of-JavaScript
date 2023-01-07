9b66ad62ee7e5a0645e71d876e34c62ddb2dbf9c
Fenix <raymundo.rojo46@gmail.com>
2023-01-07 11:36:57 -0600

    powered through Day 5. did not do all the questions but felt confident working with Arrays

--- 05_Day_Arrays/05_day_starter/scripts/main.js
+++ 05_Day_Arrays/05_day_starter/scripts/main.js
@@ -1,3 +1,29 @@
+// alert('Open the browser console whenever you work on JavaScript')
+// alert('Open the console and check if the countries has been loaded')
 console.log(countries)
-alert('Open the browser console whenever you work on JavaScript')
-alert('Open the console and check if the countries has been loaded')
+let empty=[]
+let filled_array=Array(5).fill('X')
+console.log(filled_array)
+
+let text ='I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+let words = text.split(' ')
+console.log(words)
+console.log(words.length)
+
+const shoppingCart= ['Milk', 'Coffee', 'Tea', 'Honey']
+shoppingCart.unshift('Meat')
+shoppingCart.push('Sugar')
+console.log(shoppingCart)
+shoppingCart.splice(shoppingCart.indexOf('Honey'),1)
+console.log(shoppingCart)
+shoppingCart[shoppingCart.indexOf('Tea')]='Green Tea'
+console.log(shoppingCart)
+
+if (countries.includes('Ethiopia')){
+    console.log('Ethiopia is in the Countries array')
+}
+
+webTechs.includes('SASS')
+? console.log('SASS is in webTechs')
+: webTechs.push('SASS'); console.log(webTechs)
+ 
